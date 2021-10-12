e03178063a3926819f4372e5c633576e28b5a2b6
braydondudley-nttdata <Braydon.Dudley@nttdata.com>
2021-10-12 14:34:51 -0500

    React-Redux w/out Toolkit

--- Complete Course (Max)/04_Redux/01-redux-intro-starting-project/src/components/Counter.js
+++ Complete Course (Max)/04_Redux/01-redux-intro-starting-project/src/components/Counter.js
@@ -1,12 +1,37 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+  const showCounter = useSelector(state => state.showCounter);
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment'});
+  }
+
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: 10 });
+  }
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement'});
+  }
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment By 10</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
---
+++ Complete Course (Max)/04_Redux/01-redux-intro-starting-project/src/store/index.js
@@ -0,0 +1,39 @@
+// const redux = require('redux');
+import { createStore } from 'redux';
+
+const initialState = { counter: 0, showCounter: true };
+
+// Reducer
+const reducerFn = (state = initialState, action) => {
+    if (action.type === 'increment') {
+        return {
+            counter: state.counter + 1,
+            showCounter: state.showCounter,
+        };
+    }
+    if (action.type === 'increase') {
+        return {
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter,
+        };
+    }
+    if (action.type === 'decrement') { 
+        return {
+            counter: state.counter - 1,
+            showCounter: state.showCounter,
+        };
+    }
+    if (action.type === 'toggle') {
+        return {
+            counter: state.counter,
+            showCounter: !state.showCounter,
+        }
+    }
+    return state;
+}
+
+// Store
+const store = createStore(reducerFn)
+
+// export store for provider in main index file
+export default store;
